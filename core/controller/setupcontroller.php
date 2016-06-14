a7ea3115af8d69ae4d7d6a90b3627441eb494a8d
Kyle Fazzari <kyle@canonical.com>
2016-06-14 10:26:11 -0400

    Add support for out-of-app config directory.
    If the NEXTCLOUD_CONFIG_DIR environment variable is set, it'll be
    used as the config directory.
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- core/controller/setupcontroller.php
+++ core/controller/setupcontroller.php
@@ -39,7 +39,12 @@ class SetupController {
 	 * @param Setup $setupHelper
 	 */
 	function __construct(Setup $setupHelper) {
-		$this->autoConfigFile = \OC::$SERVERROOT.'/config/autoconfig.php';
+		$config_directory = getenv('NEXTCLOUD_CONFIG_DIR');
+		if ($config_directory) {
+			$this->autoConfigFile = $config_directory.'/autoconfig.php';
+		} else {
+			$this->autoConfigFile = \OC::$SERVERROOT.'/config/autoconfig.php';
+		}
 		$this->setupHelper = $setupHelper;
 	}
 
